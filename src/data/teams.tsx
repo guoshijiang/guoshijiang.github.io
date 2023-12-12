19485d07fb16ec2ae7f40c6bb78393c65b35c1af
guoshijiang <guoshijiang2012@163.com>
2023-12-12 09:32:43 +0800

    add develop info

--- src/data/teams.tsx
+++ src/data/teams.tsx
@@ -12,11 +12,20 @@ const Teams = [
     }),
   },
   {
+    imgUrl: 'https://avatars.githubusercontent.com/u/16287657?v=4',
+    name: 'sailormillet',
+    title: translate({
+      id: 'homepage.teams.sailormillet.title',
+      message: 'Frontend Developer',
+      description: 'Title of teams of sailormillet on the home page',
+    }),
+  },
+  {
     imgUrl: 'https://avatars.githubusercontent.com/u/40434934?v=4',
     name: 'xingyaoc',
     title: translate({
       id: 'homepage.teams.xingyaoc.title',
-      message: 'Developer',
+      message: 'Frontend Developer',
       description: 'Title of teams of xingyaoc on the home page',
     }),
   },
@@ -28,7 +37,86 @@ const Teams = [
       message: 'Developer',
       description: 'Title of teams of Jack Zhang on the home page',
     }),
+  },{
+    imgUrl: 'https://avatars.githubusercontent.com/u/2945287?v=4',
+    name: 'jhfnetboy',
+    title: translate({
+      id: 'homepage.teams.jhfnetboy.title',
+      message: 'Developer',
+      description: 'Title of teams of jhfnetboy on the home page',
+    }),
+  },
+  {
+    imgUrl: 'https://avatars.githubusercontent.com/u/6221737?v=4',
+    name: 'benjamintshi',
+    title: translate({
+      id: 'homepage.teams.benjamintshi.title',
+      message: 'NodeJs Developer',
+      description: 'Title of teams of benjamintshi on the home page',
+    }),
+  },
+  {
+    imgUrl: 'https://avatars.githubusercontent.com/u/35363659?v=4',
+    name: 'tiantianlikeu',
+    title: translate({
+      id: 'homepage.teams.tiantianlikeu.title',
+      message: 'Backend Developer',
+      description: 'Title of teams of tiantianlikeu on the home page',
+    }),
+  },
+  {
+    imgUrl: 'https://avatars.githubusercontent.com/u/61449410?v=4',
+    name: 'zpano',
+    title: translate({
+      id: 'homepage.teams.zpano.title',
+      message: 'Solidity Developer',
+      description: 'Title of teams of zpano on the home page',
+    }),
+  },
+  {
+    imgUrl: 'https://avatars.githubusercontent.com/u/97324405?v=4',
+    name: 'shivanshu814',
+    title: translate({
+      id: 'homepage.teams.shivanshu814.title',
+      message: 'frontend Developer',
+      description: 'Title of teams of shivanshu814 on the home page',
+    }),
+  },
+  {
+    imgUrl: 'https://avatars.githubusercontent.com/u/97324405?v=4',
+    name: 'shivanshu814',
+    title: translate({
+      id: 'homepage.teams.shivanshu814.title',
+      message: 'frontend Developer',
+      description: 'Title of teams of shivanshu814 on the home page',
+    }),
+  },
+  {
+    imgUrl: 'https://avatars.githubusercontent.com/u/150229699?v=4',
+    name: 'buildevm',
+    title: translate({
+      id: 'homepage.teams.buildevm.title',
+      message: 'backend Developer',
+      description: 'Title of teams of buildevm on the home page',
+    }),
+  },
+  {
+    imgUrl: 'https://avatars.githubusercontent.com/u/152005203?v=4',
+    name: 'abandonsz',
+    title: translate({
+      id: 'homepage.teams.abandonsz.title',
+      message: 'Developer',
+      description: 'Title of teams of abandonsz on the home page',
+    }),
+  },
+  {
+    imgUrl: 'https://avatars.githubusercontent.com/u/152969444?v=4',
+    name: 'KeaneHh',
+    title: translate({
+      id: 'homepage.teams.abandonsz.title',
+      message: 'Backend Developer',
+      description: 'Title of teams of abandonsz on the home page',
+    }),
   },
 ];
-
 export default Teams;
